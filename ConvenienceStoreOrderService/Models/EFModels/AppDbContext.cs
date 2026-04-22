b495ed1195d995467f42955b37951bd147e90473
leianzhan <leianzhan@gmail.com>
2026-04-22 18:53:04 +0800

    建立AppDbContext、建立ProductEFModel三個欄位

---
+++ ConvenienceStoreOrderService/Models/EFModels/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ConvenienceStoreOrderService.Models.EFModels
+{
+    public class AppDbContext : DbContext
+    {
+       
+        
+          public AppDbContext() : base("name=AppDbContext")
+            {
+            }
+
+            public DbSet<ProductEFModel> Products { get; set; }
+        
+    }
+}
---
+++ ConvenienceStoreOrderService/Models/EFModels/ProductEFModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConvenienceStoreOrderService.Models.EFModels
+{
+    public class ProductEFModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+    }
+}
