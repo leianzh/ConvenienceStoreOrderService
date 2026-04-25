cc37a1b35aa080e536cdd12fd4852eb8666f7766
leianzhan <leianzhan@gmail.com>
2026-04-25 23:21:35 +0800

    EF不要自動建資料庫、不要自動改資料表

--- ConvenienceStoreOrderService/Global.asax.cs
+++ ConvenienceStoreOrderService/Global.asax.cs
@@ -1,10 +1,13 @@
+using ConvenienceStoreOrderService.Models.EFModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
 
 namespace ConvenienceStoreOrderService
 {
@@ -17,6 +20,8 @@ namespace ConvenienceStoreOrderService
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             UnityConfig.RegisterComponents();//專案啟動，把DI規則載入
+            
+            Database.SetInitializer<AppDbContext>(null);//EF 不自動建立或改資料庫。
         }
     }
 }
