de0483d11e04705f45d7a02935682504185b04d5
leianzhan <leianzhan@gmail.com>
2026-04-26 23:30:24 +0800

    Repo的Product假資料換成DB

--- ConvenienceStoreOrderService/Repositories/ProductRepository.cs
+++ ConvenienceStoreOrderService/Repositories/ProductRepository.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ConvenienceStoreOrderService.Models.EFModels;
 using ConvenienceStoreOrderService.Models.ViewModels;
 using ConvenienceStoreOrderService.Repositories.Interfaces;
+
 
 namespace ConvenienceStoreOrderService.Repositories
 {
@@ -11,12 +13,17 @@ namespace ConvenienceStoreOrderService.Repositories
     {
         public List<ProductViewModel> GetProducts()
         {
-            return new List<ProductViewModel>
+            using(var db= new AppDbContext())
             {
-                new ProductViewModel { ProductId = 1, ProductName = "紅茶", Price = 30 },
-                new ProductViewModel { ProductId = 2, ProductName = "奶茶", Price = 45 },
-                new ProductViewModel { ProductId = 3, ProductName = "綠茶", Price = 25 }
-            };
+                return db.Products
+                .Select(p => new ProductViewModel
+                {
+                    ProductId = p.ProductId,
+                    ProductName = p.ProductName,
+                    Price = p.Price
+                })
+                .ToList();
+            }
         }
     }
 }
