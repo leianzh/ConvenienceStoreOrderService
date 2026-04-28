ef1a94c908a2371a45503fdf31efe4defe2e39b7
leianzhan <leianzhan@gmail.com>
2026-04-28 20:15:17 +0800

    Nuget加上 Migration、根據EFModel在sql中產生新db、資料表

---
+++ ConvenienceStoreOrderService/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ConvenienceStoreOrderService.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ConvenienceStoreOrderService.Models.EFModels.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ConvenienceStoreOrderService.Models.EFModels.AppDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
