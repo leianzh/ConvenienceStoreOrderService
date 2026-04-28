41383cfd77c33857a20ac333a2a7c98823e272f7
leianzhan <leianzhan@gmail.com>
2026-04-29 00:15:28 +0800

    migration測試：用PMC 指令更改欄位Description->PDescription、塞入測試資料網站顯示成功

---
+++ ConvenienceStoreOrderService/Migrations/202604281551166_AlertPDescription.cs
@@ -0,0 +1,20 @@
+﻿namespace ConvenienceStoreOrderService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AlertPDescription : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "PDescription", c => c.String());
+            DropColumn("dbo.Products", "Description");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Products", "Description", c => c.String());
+            DropColumn("dbo.Products", "PDescription");
+        }
+    }
+}
--- ConvenienceStoreOrderService/Models/EFModels/Product.cs
+++ ConvenienceStoreOrderService/Models/EFModels/Product.cs
@@ -12,6 +12,6 @@ namespace ConvenienceStoreOrderService.Models.EFModels
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int Price { get; set; }
-        public string Description { get; set; }
+        public string PDescription { get; set; }
     }
 }
