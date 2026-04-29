c09b031dd02d5fc8a7263af3928c23b86b1e0a2b
leianzhan <leianzhan@gmail.com>
2026-04-29 22:37:12 +0800

    用SEED在PaymentStatus表和OrderStatus表的欄位塞資料

--- ConvenienceStoreOrderService/Migrations/Configuration.cs
+++ ConvenienceStoreOrderService/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 ﻿namespace ConvenienceStoreOrderService.Migrations
 {
+    using ConvenienceStoreOrderService.Models.EFModels;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,10 +15,21 @@
 
         protected override void Seed(ConvenienceStoreOrderService.Models.EFModels.AppDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            context.PaymentStatuses.AddOrUpdate(
+        x => x.PaymentStatusCode,
+        new PaymentStatus { PaymentStatusCode = "Pending", PaymentStatusName = "待付款", PaymentStatusSort = 1, IsActive = true },
+        new PaymentStatus { PaymentStatusCode = "Paid", PaymentStatusName = "已付款", PaymentStatusSort = 2, IsActive = true },
+        new PaymentStatus { PaymentStatusCode = "Failed", PaymentStatusName = "付款失敗", PaymentStatusSort = 3, IsActive = true },
+        new PaymentStatus { PaymentStatusCode = "Cancelled", PaymentStatusName = "已取消", PaymentStatusSort = 4, IsActive = true }
+    );
+            context.OrderStatuses.AddOrUpdate(
+        x => x.OrderStatusCode,
+        new OrderStatus { OrderStatusCode = "Processing", OrderStatusName = "處理中", OrderStatusSort = 1, IsActive = true },
+        new OrderStatus { OrderStatusCode = "Shipped", OrderStatusName = "已出貨", OrderStatusSort = 2, IsActive = true },
+        new OrderStatus { OrderStatusCode = "Arrived", OrderStatusName = "已到店", OrderStatusSort = 3, IsActive = true },
+        new OrderStatus { OrderStatusCode = "PickedUp", OrderStatusName = "已取貨", OrderStatusSort = 4, IsActive = true },
+        new OrderStatus { OrderStatusCode = "Cancelled", OrderStatusName = "已取消", OrderStatusSort = 5, IsActive = true }
+    );
         }
     }
 }
