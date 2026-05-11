9efcb1ca549a40e2e015410d3f57a859ac315bca
leianzhan <leianzhan@gmail.com>
2026-05-11 13:32:05 +0800

    加ErrorCodes

---
+++ ConvenienceStoreOrderService/Models/Common/ErrorCodes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConvenienceStoreOrderService.Models.Common
+{
+    public static class ErrorCodes
+    {
+        public const string Validation = "Validation";
+        public const string NotFound = "NotFound";
+        public const string Conflict = "Conflict";
+        public const string SystemError = "SystemError";
+    }
+}
