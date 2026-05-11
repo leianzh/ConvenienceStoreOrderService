7f080fce2fe4fe6fa7ebeeaaaabab4a35260b409
leianzhan <leianzhan@gmail.com>
2026-05-11 11:31:09 +0800

    Result<T> class

---
+++ ConvenienceStoreOrderService/Models/Common/Result.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConvenienceStoreOrderService.Models.Common
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+
+        public T Data { get; set; }
+
+        public string ErrorCode { get; set; }
+
+        public string Message { get; set; }
+        //成功時回傳
+        public static Result<T> Success(T data, string message = "")
+        {
+            return new Result<T>
+            {
+                IsSuccess = true,
+                Data = data,
+                ErrorCode = null,
+                Message = message
+            };
+        }
+        //失敗時回傳
+        public static Result<T> Fail(string errorCode, string message)
+        {
+            return new Result<T>
+            {
+                IsSuccess = false,
+                Data = default(T),
+                ErrorCode = errorCode,
+                Message = message
+            };
+        }
+    }
+}
