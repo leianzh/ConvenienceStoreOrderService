d5a1a2bf9caeb577e32a752f252b5114252ce5a7
leianzhan <leianzhan@gmail.com>
2026-07-15 21:32:13 +0800

    新增藍新單筆交易查詢API

---
+++ ConvenienceStoreOrderService/Models/DTOs/NewebPayQueryRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConvenienceStoreOrderService.Models.DTOs
+{
+    public class NewebPayQueryRequestDto
+    {
+        public string MerchantID { get; set; }
+        public string Version { get; set; }
+        public string RespondType { get; set; }
+        public string CheckValue { get; set; }
+        public string TimeStamp { get; set; }
+        public string MerchantOrderNo { get; set; }
+        public string Amt { get; set; }
+    }
+}
